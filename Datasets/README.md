28c978b1b5e70665364f225a12f22bcd8e9612fa
Javier Carrillo <stringparser@gmail.com>
2015-02-21 21:43:51 +0100

    related to #1: concat all dgt/*.csv files; take out trash .raw;

---
+++ Datasets/README.md
@@ -0,0 +1,5 @@
+## What files here are
+
+* dgt/allConcatjs.csv
+
+A `csv` all the other `csv` files under the same dir. Made with `node concat.js`. If you have node installed first do `npm install` and then `node concat.js`.
---
+++ Datasets/concat.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var through = require('through');
+var readline = require('readline');
+var outputFile = fs.createWriteStream('dgt/allConcatjs.csv');
+
+fs.readdir('dgt', function(err, dirls){
+  if(err){ throw err; }
+  dirls.forEach(function(inputFile){
+    if(inputFile !== 'allConcatjs.csv' && inputFile.match(/csv$/)){
+      cleanUp(inputFile);
+    }
+  });
+});
+
+// add more stuff here if you wish
+var sanityMap = {
+  'á': 'a',
+  'é': 'e',
+  'í': 'i',
+  'ó': 'o',
+  'ú': 'u',
+  'ü': 'u'
+};
+
+function cleanUp(inputFile){
+  var reader = readline.createInterface({
+    input: fs.createReadStream('dgt/'+inputFile),
+    output: through()
+  });
+
+  reader.on('line', function(line){
+    var saneRE = new RegExp(
+      Object.keys(sanityMap).join('|'), 'ig'
+    );
+
+    var num = line.match(/^\d+/);
+    if(num && line.match(/[ ]+/)){
+      // replace space(s) ocurrence(s) with ';'
+      line = line.trim().replace(/[ ]+/g, ';');
+    }
+
+    // ok, normalize this thing
+    line = line.toLowerCase()
+      .replace(saneRE, function($0){
+        return sanityMap[$0] || $0;
+      });
+
+    var label = inputFile.substring(1)
+      .replace('.csv', '').toLowerCase();
+
+    outputFile.write(label + ';' + line + '\n');
+  });
+}
