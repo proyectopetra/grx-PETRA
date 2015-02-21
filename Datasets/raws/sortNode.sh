2bef75a640cba9e213d0007955b0266f0dc636d3
helfio <elfio@hiperones.es>
2015-02-21 13:39:15 +0100

    Minor fixes.

--- Datasets/raws/sortNode.sh
+++ Datasets/raws/sortNode.sh
@@ -4,9 +4,9 @@ cat $1 | tr " " "_" | tr "," " " > datos.dat
 
 for i in `cat datos.dat | grep -v "idNodo" | awk '{print $1}'`
     do
-    fn=$i".datos"
+    fn=$i".csv"
     if [ ! -f $fn ]; then
-        echo "idNodo idDispositivo tfin tinicio tdiferencia latitud longitud majordeviceclass minordeviceclass serviceclass fabricante" > $fn
+        echo "idNodo,idDispositivo,tfin tinicio,tdiferencia,latitud,longitud,majordeviceclass,minordeviceclass,serviceclass,fabricante" > $fn
         cat datos.dat | grep $i | tr " " "," | tr "_" " " >> $fn
     fi
 done
