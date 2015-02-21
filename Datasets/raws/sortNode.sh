09d09a9047065ae26c622b62c886515b280a78a9
helfio <elfio@hiperones.es>
2015-02-21 13:29:49 +0100

    Added a script to strip the week data into separates nodes and the data already separated

---
+++ Datasets/raws/sortNode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat $1 | tr " " "_" | tr "," " " > datos.dat
+
+for i in `cat datos.dat | grep -v "idNodo" | awk '{print $1}'`
+    do
+    fn=$i".datos"
+    if [ ! -f $fn ]; then
+        echo "idNodo idDispositivo tfin tinicio tdiferencia latitud longitud majordeviceclass minordeviceclass serviceclass fabricante" > $fn
+        cat datos.dat | grep $i | tr " " "," | tr "_" " " >> $fn
+    fi
+done
+rm datos.dat
