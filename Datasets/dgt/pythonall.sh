ad7b9ad63a3992350266789b7e67f76e49c30b65
fergunet <fergunet@gmail.com>
2015-02-22 14:58:31 +0100

    Generated all.csv with correct information. References #17 #10 and closes #1
    Fixed one typo in HPermanentes.csv (PoblacionA: -->Poblacion:A)
    Added header.txt
    The file pythonall.sh calls processCSV.py

---
+++ Datasets/dgt/pythonall.sh
@@ -0,0 +1,7 @@
+
+cat header.txt
+python processCSV.py HPeajes.csv		
+python processCSV.py HPermanentes.csv	
+python processCSV.py HPrimarias.csv		
+python processCSV.py HSecundarias.csv	
+python processCSV.py HSemipermanentes.csv
